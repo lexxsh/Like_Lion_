19624ba098b7a6b4b013c6e44316a36d9f6018fb
lex_xsh <john8538@naver.com>
2023-03-31 20:16:15 +0900

    eazy

--- task2.js
+++ task2.js
@@ -1,1 +1,13 @@
 /* 2번 문제 답안을 작성해주세요. */
+function solution(num_list) {
+    var answer = [];
+    let hol=0;
+    let jjak=0;
+    for(let i = 0;i<num_list.length;i++){
+        if((num_list[i]%2)==0) jjak++;
+        else hol++;
+    }
+    answer[0] =jjak;
+    answer[1] =hol;
+    return answer;
+}
