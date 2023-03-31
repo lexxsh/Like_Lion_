bfe40ded52eb5360ee8793aa15fd2bf1cae980d1
lex_xsh <john8538@naver.com>
2023-03-31 20:18:48 +0900

    eazy

--- task3.js
+++ task3.js
@@ -1,1 +1,13 @@
 /* 3번 문제 답안을 작성해주세요. */
+function solution(n) {
+    var answer = 0;
+    let gop=1;
+    for(let i = 1;i<11;i++){
+        gop*=i;
+        if(gop>n) {
+            answer =  i-1;
+            break;
+        }
+    }
+    return answer;
+}
