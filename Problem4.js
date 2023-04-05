067cd118d59ff7b8434f3bbb77c30bf22ee41e81
lex_xsh <john8538@naver.com>
2023-04-05 20:14:15 +0900

    js 과제

--- Problem4.js
+++ Problem4.js
@@ -4,9 +4,8 @@ let user = {
   Name: "지환",
   years: 23,
 };
-
-// 아래에 코드를 작성해주세요 ~
-
+let{Name:Name,years:age,isAdmin}=user;
+if(isAdmin == '' || isAdmin == null) isAdmin = false;
 alert(Name); // "지환"
 alert(age); // 23
 alert(isAdmin); // false
--- Problem5.js
+++ Problem5.js
@@ -4,3 +4,8 @@ let user = {
   name: "윤정",
   age: 20,
 };
+let json = JSON.stringify(user);
+alert(json);
+let ans = JSON.parse(json);
+alert(ans.name);
+alert(ans.age);
