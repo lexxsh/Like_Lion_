b80eed86004fafe609a7e63f21daed13b9a1af91
lex_xsh <john8538@naver.com>
2023-04-05 20:21:07 +0900

    js 응용 상혀기

--- Problem4.js
+++ Problem4.js
@@ -4,8 +4,7 @@ let user = {
   Name: "지환",
   years: 23,
 };
-let{Name:Name,years:age,isAdmin}=user;
-if(isAdmin == '' || isAdmin == null) isAdmin = false;
+let{Name:Name,years:age,isAdmin=false}=user;
 alert(Name); // "지환"
 alert(age); // 23
 alert(isAdmin); // false
