eaa7f8580268eb0c9f5d7f2f0cff6c5f1ae06b08
lex_xsh <john8538@naver.com>
2023-04-26 21:02:06 +0900

    끝

--- src/components/Diary.jsx
+++ src/components/Diary.jsx
@@ -1,19 +1,44 @@
 import React, {useState} from "react";
 
+
 const Diary = () => {
-  // 이벤트 핸들러부터 state까지 모두 스스로 작성해보시길 바랍니다!
-  // 과연 어떤 state를 생성해주어야하고, 또 어떤 이벤트 핸들러가 필요할까요?
-  // 실습으로 다뤘던 두 문제를 참고하면 충분히 해결할 수 있으실거라 생각합니다!
+    const[detail,setdetail]=useState({
+      subject: '',
+      content: '',
+    });
+    
+    
+    
+    const handleValue = (e) => {
+      const {value,name} = e.target;
+
+      const nextInpusts={
+        ...detail,
+        [name]: value,
+      };
+
+      setdetail(nextInpusts);
+    }
+
+    const contentClik=()=>{
+      alert(detail.content);
+    }
+    const onReset=(e)=>{
+      setdetail({
+        subject: '',
+        content: '',
+      })
+    };
 
   return (
     <div className="diary">
-      <input name="subject" placeholder="제목을 입력해주세요" />
+      <input name="subject" placeholder="제목을 입력해주세요" onChange={handleValue} value={detail.subject}/>
       <br />
-      <textarea name="content" placeholder="내용을 입력해주세요" />
+      <textarea name="content" placeholder="내용을 입력해주세요" onChange={handleValue} />
       <br />
-      <button>초기화</button>
+      <button onClick={onReset}>초기화</button>
       <br />
-      <button>입력 완료</button>
+      <button onClick={contentClik}>입력 완료</button>
     </div>
   );
 };
--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,22 +1,23 @@
 import React, {useState} from "react";
 
-const Name = () => {
-  // 이곳에 useState 구문을 작성해주세요
+const Name = (event) => {
+    const[myName,setName]=useState(' ');
+    const[nickName,setNickName]=useState(' ');
 
-  const handleName = () => {
-    // 이곳에 이름을 설정하도록 하는 코드를 작성해주세요
+  const handleName = (event) => {
+    setName(event.target.value);
   };
 
-  const handleNickName = () => {
-    // 이곳에 닉네임을 설정하도록 하는 코드를 작성해주세요
+  const handleNickName = (event) => {
+    setNickName(event.target.value);
   };
 
   return (
     <div className="name">
       <input name="myName" placeholder="이름을 입력해주세요" onChange={handleName} />
       <input name="nickName" placeholder="닉네임을 입력해주세요" onChange={handleNickName} />
-      <p>이름: {/* 여기 중괄호 안에 닉네임이 표시되도록 하는 코드를 작성해주세요 */}</p>
-      <p>닉네임: {/* 여기 중괄호 안에 닉네임이 표시되도록 하는 코드를 작성해주세요 */}</p>
+      <p>이름: {myName ==="" ?"아직 이름이 없습니다":myName}</p>
+      <p>닉네임: {nickName ==="" ?"아직 닉네임이 없습니다":nickName}</p>
     </div>
   );
 };
